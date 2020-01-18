8257cb4050c7fbb241f6be94e11a7b9e2c2491ae
Nitin Joshi <09nitin@gmail.com>
2020-01-18 23:41:15 +0530

    Added linux desktop init preparation script

---
+++ prepare_ubuntu_desktop.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install ntp
+sudo apt-get install git
+sudo apt-get install vim
+open https://www.computerhope.com/unix/uchmod.htm
+clear
+echo =======================================
+echo “Note: If using a VM for Ubunto, run Device>Insert Guest Additions CD image…”
+echo =======================================
+
