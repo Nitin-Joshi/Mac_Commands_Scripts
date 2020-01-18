3561227cf2f46b4cfc72368b74ef0f099cfa5194
Nitin Joshi <09nitin@gmail.com>
2020-01-19 00:45:56 +0530

    Updated script to set git config file with user name and email

--- prepare_ubuntu_desktop.sh
+++ prepare_ubuntu_desktop.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+clear
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install ntp
 sudo apt-get install git
 sudo apt-get install vim
-clear
+echo ------------------------------------
+echo -------- Git configuration ---------
+echo "Please enter your name: "
+read name
+git config --global user.name "$name"
+echo "Nice to meet you $name"
+echo "Please enter your email id: "
+read email
+echo "you entered : $email"
+git config --global user.email "$email"
 echo =======================================
 echo “Note: If using a VM for Ubunto, run Devices/Insert Guest Additions CD image… from menu”
 echo =======================================
